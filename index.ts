c9a06d0f8930c8e3000f53cb02e19bceeb917d5b
Sebastian Arroyo <83790801+Esgarothh@users.noreply.github.com>
2023-12-13 22:36:32 -0300

    Libp2p main menu now handles communications (#17)
    * added - libp2p basic networking node connection
    
    * feat() index repaired - now handles communication

--- index.ts
+++ index.ts
@@ -2,7 +2,9 @@ import { Block } from "./functions/src/blockchain-core/block";
 import { Chain } from "./functions/src/blockchain-core/chain";
 import { getGenesisBlock, getLastBlock, sendBlockToServer, sendChainToServer, sendTransactionToServer ,getBlockById} from "./functions/src/blockchain-core/database_logic";
 import { Wallet } from "./functions/src/blockchain-core/wallet";
-import readline from 'readline';
+import * as readline from 'readline';
+import { startNode,sendMessage, sendMessageToPeers ,setHandleMessageCallback} from './libp2p-conexion/streamer';
+
 
 
 export const initializeGenesisBlock = async () => {
@@ -95,23 +97,51 @@ console.log(`Timestamp: ${block.ts}`);
 console.log(`Nonce: ${block.nonce}`);
 console.log(`Hash: ${block.hash}`);
 }
+function clearConsole() {
+  // Clear the console based on the operating system
+  const isWindows = process.platform === "win32";
+  if (isWindows) {
+    // For Windows
+    process.stdout.write("\x1Bc");
+  } else {
+    // For other operating systems
+    console.clear();
+  }
+}
+
+setHandleMessageCallback((message) => {
+  console.log("Received message from libp2p:", message);
+  renderMenu();
+});
+
+const renderMenu = () => {
+  // Clear the terminal screen
+
+  console.log("\nMenu:");
+  console.log("1. See last block from chain");
+  console.log("2. See last block from the database");
+  console.log("3. Get genesis block from database");
+  console.log("4. Send a generic transaction");
+  console.log("5. Get block by ID");
+  console.log("6. <test> ");
+  console.log("7. Exit");
+  console.log("Choose an option (1-5): ");
+  // Display received messages
+  
+
+};
 
 const main = async () => {
     try {
       await Chain.instance.initializeChain();
       await sendChainToServer(Chain.instance.chain);
       console.log("Initialization complete");
-  
+      const response = await startNode()
+      console.clear();
+      renderMenu();
       while (true) {
-        console.log("\nMenu:");
-        console.log("1. See last block from chain");
-        console.log("2. See last block from the database");
-        console.log("3. Get genesis block from database");
-        console.log("4. Send a generic transaction");
-        console.log("5. Get block by ID");
-        console.log("6. <test> ");
-        console.log("7. Exit");
-  
+
+        renderMenu();
         const choice = await getUserInput("Choose an option (1-5): ");
   
         switch (choice) {
@@ -136,9 +166,11 @@ const main = async () => {
               const userInput = await getUserInput("Enter the block ID: ");
               const blockId = String(userInput); // Ensure blockId is a string
               await getBlockByIdFromDatabase(blockId);
-          case '6':
-            console.log("test.");
-            break;
+            case '6':
+              const message = await getUserInput('Enter the message to send: ');
+              await sendMessage(message)
+              console.log('Message sent.');
+              break;
           case '7':
             console.log("Exiting the menu.");
             process.exit(0);
